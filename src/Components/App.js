fc04a013d32f18fb7e67024014adb3e6b779e4e3
Ajit133 <dasajit697@gmail.com>
2023-01-01 13:09:23 +0600

    Add Login

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,12 @@
 import Layout from "./Layout";
 // import Home from "./pages/Home";
-import Singup from "./pages/Singup";
+// import Singup from "./pages/Singup";
+import Login from "./pages/Login";
 import '../styles/App.css';
 function App() {
   return (
     <Layout>
-      <Singup />
+      <Login />
     </Layout>
       
   );
